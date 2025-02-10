058650378590cf991468ef3f31722a9eb754a337
ROYER Teo <teo.royer@icloud.com>
2025-02-10 16:13:00 +0100

    delete csrf

--- backend/src/app/index.ts
+++ backend/src/app/index.ts
@@ -6,7 +6,6 @@ import helmet from "helmet";
 import "./middlewares/key";
 import cors from "cors";
 import dotenv from "dotenv";
-import csrf from "csurf";
 
 export const app = express();
 dotenv.config();
@@ -16,7 +15,6 @@ if (!NODE_ENV) {
     throw new Error("NODE_ENV is not defined in environment variables.");
 }
 
-// Configuration des options CORS
 const corsOptions: cors.CorsOptions = {
     origin: (origin, callback) => {
         if (process.env.NODE_ENV === "development") {
@@ -39,41 +37,11 @@ const corsOptions: cors.CorsOptions = {
     credentials: true,
 };
 
-// Middleware de sécurité
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(helmet());
 app.use(cors(corsOptions));
 app.options("*", cors(corsOptions));
 
-// Middleware CSRF Protection
-const csrfProtection = csrf({
-    cookie: {
-        httpOnly: true, // Empêche l'accès client au cookie
-        secure: process.env.NODE_ENV === "production", // Active Secure en prod
-        sameSite: "strict", // Empêche l'envoi du cookie en cross-site
-    },
-});
-
-app.use(csrfProtection);
-
-// Route pour envoyer le token CSRF au frontend
-app.get("/api/csrf-token", (req, res) => {
-    res.cookie("XSRF-TOKEN", req.csrfToken(), {
-        httpOnly: false, // Permet au client d’accéder au token CSRF
-        secure: process.env.NODE_ENV === "production",
-        sameSite: "strict",
-    });
-    res.json({ csrfToken: req.csrfToken() });
-});
-
-// Exemple de route protégée
-app.post("/api/protected", (req, res) => {
-    res.json({ message: "Requête CSRF sécurisée réussie !" });
-});
-
-// Swagger
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-
-// Gestion des erreurs
 app.use(errorHandler);

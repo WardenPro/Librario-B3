1151ee9f37dacac3baef51d91416c775523bdbac
zoxx11 <teoroyer@icloud.com>
2025-02-14 14:42:05 +0100

    update get reservation with left join

--- backend/src/routes/reservation/get.ts
+++ backend/src/routes/reservation/get.ts
@@ -1,13 +1,14 @@
 import { app } from "../../app/index";
 import { db } from "../../app/config/database";
-import {
-    reservation,
-    selectReservationSchema,
-} from "../../db/schema/reservation";
+import { reservation, selectReservationSchema } from "../../db/schema/reservation";
 import { checkTokenMiddleware } from "../../app/middlewares/verify_jwt";
 import { grantedAccessMiddleware } from "../../app/middlewares/verify_access_right";
 import { Request, Response, NextFunction } from "express";
 import { AppError } from "../../app/utils/AppError";
+import { copy } from "../../db/schema/copy";
+import { books } from "../../db/schema/book";
+import { users } from "../../db/schema/users";
+import { eq } from "drizzle-orm";
 
 app.get(
     "/reservations",
@@ -15,10 +16,38 @@ app.get(
     grantedAccessMiddleware("admin"),
     async (req: Request, res: Response, next: NextFunction) => {
         try {
-            const allReservations = await db.select().from(reservation);
-            const validatedReservations = allReservations.map((r) =>
-                selectReservationSchema.parse(r),
-            );
+            const allReservations = await db
+                .select({
+                    id: reservation.id,
+                    reservation_date: reservation.reservation_date,
+                    final_date: reservation.final_date,
+                    user_id: reservation.user_id,
+                    copy_id: reservation.copy_id,
+                    book_title: books.title,
+                    user_first_name: users.first_name,
+                    user_last_name: users.last_name,
+                })
+                .from(reservation)
+                .innerJoin(copy, eq(copy.id, reservation.copy_id))
+                .innerJoin(books, eq(books.id, copy.book_id))
+                .innerJoin(users, eq(users.id, reservation.user_id))
+                .execute();
+
+            console.log(allReservations);
+
+            const validatedReservations = allReservations.map((r) => {
+                const validatedReservation = selectReservationSchema.parse(r);
+                return {
+                    id: validatedReservation.id,
+                    user_id: validatedReservation.user_id,
+                    copy_id: validatedReservation.copy_id,
+                    reservation_date: validatedReservation.reservation_date,
+                    final_date: validatedReservation.final_date,
+                    user_first_name: r.user_first_name,
+                    user_last_name: r.user_last_name,
+                    book_title: r.book_title,
+                };
+            });
 
             res.status(200).json(validatedReservations);
         } catch (error) {

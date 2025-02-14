bf80f9bafaa40c3446767cb6507557a94edc00d5
SuperKnacky <superretards22@gmail.com>
2025-02-14 15:03:49 +0100

    check if admin account already exist insted of check if default admin email exist

--- backend/src/routes/users/create_admin.ts
+++ backend/src/routes/users/create_admin.ts
@@ -3,39 +3,45 @@ import { users } from "../../db/schema/users";
 import { eq } from "drizzle-orm";
 import { argon2id } from "hash-wasm";
 import crypto from "crypto";
+import { logMessage } from "../../app/utils/logger";
+import { insertUserSchema } from "../../db/schema/users";
 
 export async function createAdmin() {
     try {
-        const existingAdmin = await db
+        const [existingAdmin] = await db
             .select()
             .from(users)
-            .where(eq(users.email, "admin@example.com"));
-        if (existingAdmin.length === 0) {
-            const salt = new Uint8Array(16);
-            crypto.getRandomValues(salt);
-            const hashedPassword = await argon2id({
-                password: "adminpassword",
-                salt,
-                parallelism: 1,
-                iterations: 2,
-                memorySize: 19456,
-                hashLength: 32,
-                outputType: "encoded",
-            });
+            .where(eq(users.roles, "admin"));
+        if (existingAdmin)
+            return;
 
-            await db.insert(users).values({
-                last_name: "Admin",
-                first_name: "Admin",
-                password: hashedPassword,
-                email: "admin@example.com",
-                roles: "admin",
-                created_at: new Date(),
-            });
+        const adminPassword = "adminpassword";
 
-            console.log("Admin user created successfully");
-        } else {
-            console.log("Admin user already exists");
-        }
+        const salt = new Uint8Array(16);
+        crypto.getRandomValues(salt);
+        const hashedPassword = await argon2id({
+            password: adminPassword,
+            salt,
+            parallelism: 1,
+            iterations: 2,
+            memorySize: 19456,
+            hashLength: 32,
+            outputType: "encoded",
+        });
+
+        const validatedInsert = insertUserSchema.parse({
+            first_name: "Admin",
+            last_name: "Admin",
+            password: adminPassword,
+            email: "admin@example.com",
+            roles: "admin",
+        });
+
+        validatedInsert.password = hashedPassword;
+
+        await db.insert(users).values(validatedInsert).returning();
+
+        logMessage("Default admin user created successfully");
     } catch (error) {
         console.error(error);
     }

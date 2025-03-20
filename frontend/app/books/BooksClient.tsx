008b6d5b86b38e2b7e73441d3283fcf2139a2b30
zoxx11 <teoroyer@icloud.com>
2025-03-20 14:52:21 +0100

    update book and copy with status , update reservation claiming

--- frontend/app/books/BooksClient.tsx
+++ frontend/app/books/BooksClient.tsx
@@ -65,7 +65,6 @@ export default function BooksClient() {
     fetchBooks();
   }, []);
 
-  // Chargement des détails du livre et de ses copies quand bookId change
   useEffect(() => {
     const fetchBookDetails = async (id: string) => {
       try {
@@ -93,6 +92,7 @@ export default function BooksClient() {
             auth_token: `${localStorage.getItem("auth_token")}`,
           },
         });
+        console.log("REPONSE COPY", response, copies);
         if (response.ok) {
           const data: Copy[] = await response.json();
           setCopies(data);
@@ -110,13 +110,11 @@ export default function BooksClient() {
       fetchBookDetails(bookId);
       fetchCopies(bookId);
     } else {
-      // Réinitialiser lorsque l'on revient à la liste des livres
       setSelectedBook(null);
       setCopies([]);
     }
   }, [bookId]);
 
-  // Fonction helper pour définir la couleur en fonction de l'état
   const getStateColor = (state: string) => {
     switch (state.toLowerCase()) {
       case "excellent":
@@ -135,14 +133,13 @@ export default function BooksClient() {
     }
   };
 
-  // Si on a un bookId, on affiche les détails du livre et ses copies
   if (bookId) {
     return (
       <div className="container mx-auto py-6">
         <Button
           variant="ghost"
           className="mb-4 flex items-center"
-          onClick={() => router.push("/")}
+          onClick={() => router.push("/books")}
         >
           <ArrowLeft className="mr-2 h-4 w-4" /> Retour aux livres
         </Button>
@@ -240,7 +237,6 @@ export default function BooksClient() {
     );
   }
 
-  // Affichage de la liste des livres si aucun bookId n'est sélectionné
   return (
     <>
       <div className="flex justify-end mb-4">
